bb7d2fbf3b35167694a1915e711d2fb5a98713d3
miholjcicmarko <u0984549@utah.edu>
2020-10-02 19:55:27 -0600

    update

--- hw4/js/info_box.js
+++ hw4/js/info_box.js
@@ -84,7 +84,6 @@ class InfoBox {
 
         let text_indicator = d3.select("#country-detail").selectAll("text")
             .data(infobox_data)
-            .remove().enter().append()
             .attr("class", "stat")
             .attr("class", "stats span")
             .attr("class", "label")
