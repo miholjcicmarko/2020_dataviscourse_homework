7187b14fbddf45a45acbf8cf0653ec86f2e65493
miholjcicmarko <u0984549@utah.edu>
2020-10-04 22:01:51 -0600

    update

--- hw5/js/table.js
+++ hw5/js/table.js
@@ -224,7 +224,7 @@ class Table {
         /**
          * add gridlines to the vizualization
          */
-
+        debugger;
         let gridlines = d3.select("#predicitonTable").selectAll("td");
 
         gridlines.append("line")
