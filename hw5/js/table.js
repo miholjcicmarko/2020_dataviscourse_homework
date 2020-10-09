de94a598c83a198b2dec12d18149a8db572c3949
miholjcicmarko <u0984549@utah.edu>
2020-10-08 23:52:57 -0600

    update

--- hw5/js/table.js
+++ hw5/js/table.js
@@ -528,9 +528,14 @@ class Table {
 
                     debugger;
 
-                    for (let k = 0; k < new_indicies.length; k++) {
+                    for (let k = 0; k < poll_states.length; k++) {
                         for (let i = 0; i < polling_data[k][0].length; i++) {
-                            that.tableData.splice((new_indicies[k]+1), 0, polling_data[0][0][i]);
+                            if (poll_states[k] === that.tableData[k].state) {
+                                that.tableData.splice((new_indicies[k]+1), 0, polling_data[0][0][i]);
+                            }
+                            else {
+                                continue;
+                            }
                         }
                     }
 
