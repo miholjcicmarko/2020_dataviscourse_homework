90be90ce9f3f4e4d05b3c670ddea62af3c424f0f
miholjcicmarko <u0984549@utah.edu>
2020-10-13 00:28:53 -0600

    update

---
+++ hw6/js/bubblechart.js
@@ -0,0 +1,22 @@
+class BubbleChart {
+
+    /**
+     * Creates a BubbleChart Object
+     *
+     * @param data the full dataset
+     */
+    constructor(data) {
+        
+    }
+
+    /**
+     * Draws the BubbleChart
+     *
+     */
+    drawChart() {
+        
+    }
+
+
+
+}
