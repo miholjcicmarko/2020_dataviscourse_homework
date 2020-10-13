47eedf022848170e993b60d22d4beb97cae0b2f0
miholjcicmarko <u0984549@utah.edu>
2020-10-13 01:25:37 -0600

    update

--- hw6/js/script.js
+++ hw6/js/script.js
@@ -1,34 +1,30 @@
-debugger;
-let words = d3.json('./data/words.json');
-let bubbleChart = new BubbleChart(words);
-bubbleChart.drawChart();
+/**
+* Requests the file and executes a callback with the parsed result once
+* it is available
+* @param {string} path - The path to the file.
+* @param {function} callback - The callback function to execute once the result is available
+*/
+function fetchJSONFile(path, callback) {
+    debugger;
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                let data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
 
-// /**
-// * Requests the file and executes a callback with the parsed result once
-// * it is available
-// * @param {string} path - The path to the file.
-// * @param {function} callback - The callback function to execute once the result is available
-// */
-// function fetchJSONFile(path, callback) {
-//     let httpRequest = new XMLHttpRequest();
-//     httpRequest.onreadystatechange = function() {
-//         if (httpRequest.readyState === 4) {
-//             if (httpRequest.status === 200) {
-//                 let data = JSON.parse(httpRequest.responseText);
-//                 if (callback) callback(data);
-//             }
-//         }
-//     };
-//     httpRequest.open('GET', path);
-//     httpRequest.send();
-// }
-
-// // call fetchJSONFile then build and render a tree
-// // this is the function executed as a callback when parsing is done
-// fetchJSONFile('data/words.json', function(data) {
-//     debugger;
-//     let bubbleChart = new bubbleChart(data);
-//     bubbleChart.drawChart();
-//     //tree.renderTree();
-// });
+// call fetchJSONFile then build and render a tree
+// this is the function executed as a callback when parsing is done
+fetchJSONFile('data/words.json', function(data) {
+    debugger;
+    let bubbleChart = new bubbleChart(data);
+    bubbleChart.drawChart();
+    //tree.renderTree();
+});
 
