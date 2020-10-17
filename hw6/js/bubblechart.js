e7defb119c3f5ccf87d0543b7ef200e7d37b9011
miholjcicmarko <u0984549@utah.edu>
2020-10-17 11:12:07 -0600

    update

--- hw6/js/bubblechart.js
+++ hw6/js/bubblechart.js
@@ -76,14 +76,14 @@ debugger;
         
             xaxis.append("text")
                 .classed("axis-label-x", true)
-                .attr("transform", "translate("+(5*this.margin.left)+"," +(2*this.margin.top)+")")
+                .attr("transform", "translate("+(10*this.margin.left)+"," +(2*this.margin.top)+")")
                 .attr("text-anchor", "middle")
                 .attr("class", "axis-label")
                 .attr("class", "x-label");
 
             yaxis.append("text")
                 .classed("axis-label-y", true)
-                .attr("transform", "translate("+(1.2*this.margin.bottom) + ","+(2.5*this.margin.left)+")rotate(-90)")
+                .attr("transform", "translate("+(2*this.margin.bottom) + ","+(2.5*this.margin.left)+")rotate(-90)")
                 .attr("class", "axis-label")
                 .attr("text-anchor", "middle")
                 .attr("class", "y-label");  
