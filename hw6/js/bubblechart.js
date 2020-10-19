c872a30dcb14f25b5b6e35735eeaebed7f269a2b
miholjcicmarko <u0984549@utah.edu>
2020-10-19 16:18:47 -0600

    update

--- hw6/js/bubblechart.js
+++ hw6/js/bubblechart.js
@@ -140,7 +140,7 @@ class bubblechart {
             .attr("width", this.width + this.margin.left + this.margin.right)
             .attr("height", this.chartHeight + this.margin.top + this.margin.bottom);
 
-        let svgGroup = d3.select('#chart-view').select('.plot-svg').append('g').classed('wrapper-group', true);
+        let svgGroup = d3.select('#chart-view').select('.plot-svg').append('g').classed('brushes', true);
     
             let xaxis = svgGroup.append("g")
                 .classed("x-axis", true)
