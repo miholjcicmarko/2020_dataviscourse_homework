6a3f96dc9648d8ee94293d4290ad4d83245edb44
miholjcicmarko <u0984549@utah.edu>
2020-10-22 19:43:49 -0600

    update

--- hw6/js/bubblechart.js
+++ hw6/js/bubblechart.js
@@ -713,8 +713,8 @@ class bubblechart {
             let pos_val = pos * -1;
             pos = pos_val.toFixed(3);
         }
-        return "<h5>" + phrase + "<br/>" + 
-            party + " " + pos + "%" +"<br/>" + 
+        return "<h3>" + phrase + "</h3>" + "<br/>" + 
+            "</h5" + party + " " + pos + "%" +"<br/>" + 
             "In " + frequency_fixed + "%" + " of speeches" + "</h5>";
     }
 
