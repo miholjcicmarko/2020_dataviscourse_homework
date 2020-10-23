a6b3cee0919fb0843cf503b807b6090e236ad3bf
miholjcicmarko <u0984549@utah.edu>
2020-10-23 00:59:55 -0600

    update

--- hw6/js/script.js
+++ hw6/js/script.js
@@ -1,38 +1,3 @@
-// /**
-// * Requests the file and executes a callback with the parsed result once
-// * it is available
-// * @param {string} path - The path to the file.
-// * @param {function} callback - The callback function to execute once the result is available
-// */
-// function fetchJSONFile(path, callback) {
-//     let httpRequest = new XMLHttpRequest();
-//     httpRequest.onreadystatechange = function() {
-//         if (httpRequest.readyState === 4) {
-//             if (httpRequest.status === 200) {
-//                 let data = JSON.parse(httpRequest.responseText);
-//                 if (callback) callback(data);
-//             }
-//         }
-//     };
-//     httpRequest.open('GET', path);
-//     httpRequest.send();
-// }
-
-// function updateTable(newData) {
-
-// }
-
-// // call fetchJSONFile then build and render a tree
-// // this is the function executed as a callback when parsing is done
-// fetchJSONFile('data/words.json', function(data) {
-//     debugger;
-//     let bubbles = new bubblechart(data);
-//     bubbles.drawChart();
-//     let bars = new table(data);
-//     bars.drawTable();
-
-// });
-
 words_json = d3.json('data/words.json');
 
 Promise.all([words_json]).then(data => {
