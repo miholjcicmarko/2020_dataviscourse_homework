db3ee486d79bc23dd17cffad9ff88ff9b0ed570e
miholjcicmarko <u0984549@utah.edu>
2020-10-24 01:47:57 -0600

    update

--- hw6/js/bubblechart.js
+++ hw6/js/bubblechart.js
@@ -648,6 +648,8 @@ class bubblechart {
                                 .style("top", 0 + "px")
                                 .style("left", 0 + "px");
 
+                d3.select('#dem-Extreme').classed("blueChosen", true);
+
                 drDiv.append("div")
                     .attr("id", "textDem")
                     .style("width", "100px")
