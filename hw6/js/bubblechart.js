0672a6c63b76f0413a7b2636cf16b49a34ed77ed
miholjcicmarko <u0984549@utah.edu>
2020-10-24 03:14:12 -0600

    update

--- hw6/js/bubblechart.js
+++ hw6/js/bubblechart.js
@@ -335,8 +335,7 @@ class bubblechart {
                             .attr("y", (d,i) => 50+(i*140));
 
         if (this.isExpanded) {
-            catLabels.attr("opacity", 0.8)
-                .classed("catlabel", true);
+            catLabels.attr("opacity", 0.8);
         }
         else if (this.isExpanded === false) {
             catLabels.attr("opacity", 0.8)
